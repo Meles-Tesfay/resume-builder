ab5b4bd4ca54e623552c7fd7f03693e754d81dea
Meles-Tesfay <mandatory279@gmail.com>
2025-12-21 11:12:11 +0300

    Fix case-sensitivity build error in Home.jsx

--- client/src/paegs/Home.jsx
+++ client/src/paegs/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner from "../components/Home/Banner";
+import Banner from "../components/home/Banner";
 import Hero from "../components/home/Hero";
 import Features from "./Features";
 import Testimonial from "../components/home/Testimonial";
